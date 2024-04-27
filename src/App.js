a2c05237d596add199339919a15db950462fe7bb
Swati <jswati603@gmail.com>
2024-04-27 12:31:42 +0530

    product

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import {LinkContainer} from "react-router-bootstrap"
 function App() {
   return (
   <BrowserRouter>
-    <div className="d-flex flex-column site container">
+    <div className="d-flex flex-column site-container">
       <header>
         <Navbar bg="dark" variant="dark">
           <Container>
@@ -21,10 +21,12 @@ function App() {
         </Navbar>
       </header>
       <main>
+        <Container>
         <Routes>
           < Route path="/product/:slug" element={<ProductScreen/>}/>
           <Route path="/" element={<HomeScreen/>}/>
         </Routes>
+        </Container>
         </main>
         <footer>
           <div className="text-center">All rights reserved</div>
