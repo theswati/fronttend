de4d3a121eca0a49c75dd3eaeeacca79b23d67cc
Swati <jswati603@gmail.com>
2024-05-07 11:33:49 +0530

    cartscreen

--- src/App.js
+++ src/App.js
@@ -148,3 +148,9 @@ export default App;
 // 1. create 2 columns
 //  2.display items list
 // .3. create action column
+
+//Task-12
+// Complete Cart Screen
+// 1. click handler for inc/dec item
+// 2. click handler for remove item
+// 3. click handler for checkout
--- src/Store.js
+++ src/Store.js
@@ -8,7 +8,8 @@ export const Store=createContext()
 //usereducer
 const initialState={
     cart:{
-        cartItems:[]
+        cartItems:localStorage.getItem('cartItems')
+        ?JSON.parse(localStorage.getItem('cartItems')):[],
     }
 };
 
@@ -37,11 +38,23 @@ function reducer(state,action){
             item._id===existItem._id?newItem:item
         )
         :[...state.cart.cartItems,newItem];
+        localStorage.setItem('cartItems',JSON.stringify(cartItems));
             
         return{
                 ...state,
                 cart:{...state.cart, cartItems},
             };
+
+            case 'CART_REMOVE_ITEM':{
+                const cartItems=state.cart.cartItems.filter(
+                    (item)=>item._id!==action.payload._id
+                );
+                localStorage.setItem('cartItems',JSON.stringify(cartItems));
+                return{
+                    ...state,
+                    cart:{...state.cart, cartItems},
+                };
+            }
             default:
                 return state;
     }
