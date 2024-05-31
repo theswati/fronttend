37518f73cd5a122e99c84a8fc47caa6319dc6e14
Swati <jswati603@gmail.com>
2024-05-31 10:40:24 +0530

    payment

--- src/screens/OrderScreen.js
+++ src/screens/OrderScreen.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React,{useContext,useEffect,useReducer} from 'react';
+import { PayPalButtons,usePayPalScriptReducer } from '@paypal/react-paypal-js';
 import {Helmet} from 'react-helmet-async';
 import{useNavigate,useParams}from 'react-router-dom';
 import Row from 'react-bootstrap/Row';
@@ -11,6 +12,8 @@ import LoadingBox from '../Components/LoadingBox';
 import MessageBox from '../Components/MessageBox';
 import {Store} from '../Store';
 import getError from "../utils";
+import {toast} from 'react-toastify';
+import { ListGroupItem } from 'react-bootstrap';
 
 
 function reducer(state,action){
@@ -25,6 +28,18 @@ function reducer(state,action){
     case'FETCH_FAIL':
     return{...state,loading:false,error:action.payload};
 
+    case 'PAY_REQUEST':
+        return{...state,loadingPay:true};
+
+    case 'PAY_SUCCESS':
+        return{...state,loadingPay:false,successPay:true};
+
+    case 'PAY_FAIL':
+        return{...state,loadingPay:false};
+
+    case 'PAY_RESET':
+        return{...state,loadingPay:false,successPay:false}
+
     default:
     return state;
     }
@@ -38,12 +53,53 @@ export default function OrderScreen(){
     const {id:orderId}=params;
     const navigate=useNavigate();
 
-    const [{loading,error,order},dispatch]= useReducer(reducer,{
+    const [{loading,error,order,successPay,loadingPay},dispatch]= useReducer(reducer,{
         loading:true,
         order:{},
         error:'',
+        successPay:false,
+        loadingPay:false,
     });
 
+    const [{isPending},paypalDispatch]=usePayPalScriptReducer();
+
+    function createOrder(data,actions){
+        return actions.order
+        .create({
+            purchase_units:[
+                {
+                    amount:{value:order.totalPrice}
+                }
+            ]
+        })
+        .then((orderID)=>{
+            return orderID;
+        });
+    }
+    
+
+    function onApprove(data,actions){
+        return actions.order.capture().then(async function(details){
+            try{
+                dispatch({type:'PAY_REQUEST'});
+                const {data}=await axios.put(
+                    `/api/orders/${order._id}/pay`,
+                    details,
+                    {
+                        headers:{Authorization:`Bearer $(userInfo.token)`}
+                    }
+                );
+                dispatch({type:'PAY_SUCCESS',payload:data});
+                toast.success('Order is Paid');
+            }catch(err){
+                dispatch({type:'PAY_FAIL',payload:data});
+                toast.error(getError(err));
+            }
+        })
+    }
+    function onError(err){
+        toast.error(getError(err));
+    }
     useEffect(()=>{
         const fetchOrder=async()=>{
             try{
@@ -61,10 +117,28 @@ export default function OrderScreen(){
             return navigate('/login');
         }
 
-        if(!order._id||(order._id && order._id!==orderId)){
+        if(!order._id||successPay||(order._id && order._id!==orderId)){
             fetchOrder();
+            if (successPay){
+                dispatch({type:'PAY_RESET'})
+            }
+        }else{
+            const loadPaypalScript=async()=>{
+                const {data:clientId}=await axios.get('/api/keys/paypal',{
+                    headers:{authorization:`Bearer ${userInfo.token}`},
+                });
+                paypalDispatch({
+                    type:'resetOptions',
+                    value:{
+                        'client-Id':clientId,
+                        currency:'USD'
+                    }
+                });
+                paypalDispatch({type:'setLoadingStatus',value:'pending'});
+            };
+            loadPaypalScript();
         }
-    },[order,userInfo,orderId,navigate]);
+    },[order,userInfo,orderId,navigate,paypalDispatch,successPay]);
 
 
 return loading?(
@@ -181,6 +255,23 @@ return loading?(
                                     </Col>
                                 </Row>
                             </ListGroup.Item>
+
+                            {!order.isPaid && (
+                                <ListGroup.Item>
+                                    {isPending?(
+                                        <LoadingBox/>
+                                    ):(
+                                        <div>
+                                            <PayPalButtons
+                                            createOrder={createOrder}
+                                            onApprove={onApprove}
+                                            onError={onError}
+                                            ></PayPalButtons>
+                                        </div>
+                                    )}
+                                    {loadingPay && <LoadingBox></LoadingBox>}
+                                </ListGroup.Item>
+                            )}
                         </ListGroup>
                     </Card.Body>
                 </Card>
